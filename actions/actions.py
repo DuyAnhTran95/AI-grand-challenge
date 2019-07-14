0c5e76707ade6300c622485d355d0cd4ffc2ab75
Duy Anh <duyanh.tran8995@gmail.com>
2019-07-14 08:43:18 +0700

    fix action

--- actions/actions.py
+++ actions/actions.py
@@ -1,61 +1,116 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+# -*- coding: utf-8 -*-
+from typing import Dict, Text, Any, List, Union, Optional
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 
 
-class ActionHelloWorld(Action):
+class DocumentForm(FormAction):
+    """Example of a custom form action"""
 
     def name(self) -> Text:
-        return "action_hello_world"
+        """Unique identifier of the form"""
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message("Hello World!")
+        return "document_form"
 
-        return []
-
-
-class TimTaiLieuAction(Action):
-    def name(self) -> Text:
-        return "action_tim_tai_lieu"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # dispatcher.utter_message("Hello World!")
-        print('get intent tim tai lieu')
-        muc_dich_slot = next(tracker.get_latest_entity_values("muc_dich"), None)
-        if muc_dich_slot is None:
-            dispatcher.utter_template("utter_ask_muc_dich", tracker)
-        else:
-            trinh_do_slot = next(tracker.get_latest_entity_values("trinh_do"), None)
-            print(trinh_do_slot)
-            if trinh_do_slot is None:
-                dispatcher.utter_template("utter_ask_trinh_do", tracker)
-            else:
-                dispatcher.utter_message("https://cfl.edu.vn/tag/danh-sach-thi-toeic-quoc-te-iig/")
-            return []
-
-
-class TimTaiLieuForm(FormAction):
-
-    def name(self) -> Text:
-        return "tim_tai_lieu_form"
-
+    @staticmethod
     def required_slots(tracker: Tracker) -> List[Text]:
-        return ['muc_dich', 'trinh_do']
+        """A list of required slots that the form has to fill"""
 
-    def submit(self, dispatcher, tracker, domain) -> List[Dict]:
+        return ["level", "type_exam"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+
+        return {
+            "level": 
+            self.from_entity(entity="level", not_intent="chitchat"),
+            "type_exam": 
+            self.from_entity(
+                    entity="type_exam", intent=["inform", "request_document"])
+        }
+
+    # USED FOR DOCS: do not rename without updating in docs
+    @staticmethod
+    def level_db() -> List[Text]:
+        """Database of supported level"""
+
+        return [
+            "beginner",
+            "intermediate",
+            "good"
+        ]
+
+    @staticmethod
+    def type_exam_db() -> List[Text]:
+        return [
+            "toeic",
+            "ielts",
+            "toefl"
+        ]
+
+    @staticmethod
+    def is_int(string: Text) -> bool:
+        """Check if a string is an integer"""
+
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+
+    # USED FOR DOCS: do not rename without updating in docs
+    def validate_level(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Optional[Text]:
+        """Validate cuisine value."""
+
+        if value.lower() in self.level_db():
+            # validation succeeded, set the value of the "cuisine" slot to value
+            return {"level": value}
+        else:
+            dispatcher.utter_template("utter_wrong_level", tracker)
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"level": None}
+
+    def validate_type_exam(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Optional[Text]:
+        """Validate type_exam value."""
+
+        if value.lower() in self.type_exam_db():
+            return {"type_exam": value}
+        else:
+            dispatcher.utter_template("utter_wrong_type_exam", tracker)
+            # validation failed, set slot to None
+            return {"type_exam": None}
+
+
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do
+            after all required slots are filled"""
+        print('check okok ',tracker )
+        type_exam = tracker.get_slot('type_exam')
+        level = tracker.get_slot('level')
+        # utter submit template
+        dispatcher.utter_message("Đây là tài liệu của bạn {} và {}".format(type_exam, level))
         return []
